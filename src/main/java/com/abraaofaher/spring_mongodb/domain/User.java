ce893c1c2c648408a058cdbb9a7578d4ca92e584
Yonlero <abraaobrg150@gmail.com>
2021-07-15 00:56:20 -0300

    Conectando ao MongoDB com repository e service

--- src/main/java/com/abraaofaher/spring_mongodb/domain/User.java
+++ src/main/java/com/abraaofaher/spring_mongodb/domain/User.java
@@ -1,9 +1,14 @@
 package com.abraaofaher.spring_mongodb.domain;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 import java.io.Serializable;
 
+@Document
 public class User implements Serializable {
 
+    @Id
     private String id;
     private String name;
     private String email;
