40fd6c6f6671b3d5f49540bd93280dd493419fa5
Yonlero <abraaobrg150@gmail.com>
2021-07-21 17:12:33 -0300

    Atualização de usuário com PUT

--- src/main/java/com/abraaofaher/spring_mongodb/service/UserService.java
+++ src/main/java/com/abraaofaher/spring_mongodb/service/UserService.java
@@ -6,6 +6,7 @@ import com.abraaofaher.spring_mongodb.repository.UserRepository;
 import com.abraaofaher.spring_mongodb.service.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import java.util.List;
 import java.util.Optional;
 
@@ -15,25 +16,36 @@ public class UserService {
     @Autowired
     private UserRepository repo;
 
-    public List<User> findAll(){
+    public List<User> findAll() {
         return repo.findAll();
     }
 
-    public User findById(String id){
+    public User findById(String id) {
         Optional<User> user = repo.findById(id);
         return user.orElseThrow(() -> new ObjectNotFoundException("Rapazzzz, achei não o"));
     }
 
-    public User insert(User obj){
+    public User insert(User obj) {
         return repo.insert(obj);
     }
 
-    public void delete(String id){
+    public void delete(String id) {
         findById(id);
         repo.deleteById(id);
     }
 
-    public User fromDTO(UserDTO objDto){
+    public User update(User obj) {
+        Optional<User> newObj = repo.findById(obj.getId());
+        updateData(newObj, obj);
+        return repo.save(newObj.get());
+    }
+
+    private void updateData(Optional<User> newObj, User obj) {
+        newObj.get().setName(obj.getName());
+        newObj.get().setEmail(obj.getEmail());
+    }
+
+    public User fromDTO(UserDTO objDto) {
         return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
 }
