c051a997b1188d83a3b324cdbf9c5ce8b6df99e0
Yonlero <abraaobrg150@gmail.com>
2021-07-25 17:30:22 -0300

    Criando entity Post com User aninhado

--- src/main/java/com/abraaofaher/spring_mongodb/config/Instantiation.java
+++ src/main/java/com/abraaofaher/spring_mongodb/config/Instantiation.java
@@ -1,12 +1,17 @@
 package com.abraaofaher.spring_mongodb.config;
 
+import com.abraaofaher.spring_mongodb.domain.Post;
 import com.abraaofaher.spring_mongodb.domain.User;
+import com.abraaofaher.spring_mongodb.repository.PostRepository;
 import com.abraaofaher.spring_mongodb.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 @Configuration
 public class Instantiation implements CommandLineRunner {
@@ -14,15 +19,26 @@ public class Instantiation implements CommandLineRunner {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PostRepository postRepository;
+
     @Override
-    public void run(String... args){
+    public void run(String... args) throws ParseException {
+
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 
         userRepository.deleteAll();
+        postRepository.deleteAll();
 
         User maria = new User(null, "Maria Brown", "maria@gmail.com");
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "BobGrey", "bob@gmail.com");
 
+        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para São Paulo. Abraços!", maria);
+        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
+
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
+        postRepository.saveAll(Arrays.asList(post1, post2));
     }
 }
