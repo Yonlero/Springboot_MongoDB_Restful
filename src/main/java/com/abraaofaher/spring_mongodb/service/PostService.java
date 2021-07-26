0918f14ae7799ab20e2b01a8ab522962eafc7dc6
Yonlero <abraaobrg150@gmail.com>
2021-07-25 23:58:09 -0300

    Consulta simples com query methods

--- src/main/java/com/abraaofaher/spring_mongodb/service/PostService.java
+++ src/main/java/com/abraaofaher/spring_mongodb/service/PostService.java
@@ -6,6 +6,7 @@ import com.abraaofaher.spring_mongodb.service.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -17,6 +18,10 @@ public class PostService {
     public Post findById(String id) {
         Optional<Post> post = repo.findById(id);
         return post.orElseThrow(() -> new ObjectNotFoundException("Rapazzzz, achei não o"));
+    }
+
+    public List<Post> findByTitle(String text){
+        return repo.findByTitleContainingIgnoreCase(text);
     }
 
 }
